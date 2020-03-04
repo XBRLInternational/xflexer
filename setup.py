5157fbf478af7942a029ea3ce55548df67869333
Paul Warren <pdw@ex-parrot.com>
2020-03-04 15:43:55 +0000

    Add dependency.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(
     name = "XFLexer",
     version = "0.1",
     packages = find_packages(),
+    install_requires = [ 'pygments' ],
     entry_points = { 'pygments.lexers': 'xflexer = xflexer:XFLexer' }
 )
 
